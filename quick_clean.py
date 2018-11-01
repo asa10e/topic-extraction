61687e27e1f18bc36c13abe9a939554240abbb9d
asa10e <31635204+asa10e@users.noreply.github.com>
2018-11-01 17:32:32 +0800

    Just get rid of everything before (Reuters)

--- quick_clean.py
+++ quick_clean.py
@@ -1,19 +1,10 @@
 import re
-from unidecode import unidecode
 def quick_clean(text):
 
-    text = unidecode(text) # Replaces stuff like smart quotes with stuff like simple quotes
     text = re.sub(r'\s+', ' ', text) # Get rid of extra whitespace and newlines
+    text = re.sub(r'^(.*\(Reuters\))','',text) # Just get rid of everything before the (Reuters)
 
     text = text.replace('For best results when printing this announcement, please click on link below:', ' ')
-    text = re.sub(r'^\((.*?)\)',' ', text) # Remove ^('text')
-
-    text = text.lstrip() # Trim leading whitespace, which allows the following few lines to work
-    text = re.sub(r'^By\s[a-zA-Z]+\s[a-zA-Z]+\s', '', text) # Removes ^By Asa Tenney
-    text = re.sub(r'^[a-zA-Z]+\s+[a-zA-Z]+,\s[a-zA-Z]{3}\s\d{1,2}', ' ', text) # Removes ^VATICAN CITY, Jan 26
-    text = re.sub(r'^[a-zA-Z]+\s+[a-zA-Z]+\s\(Reuters\)', ' ', text)
-    text = re.sub(r'^[a-zA-Z]+,\s[a-zA-Z]{3,8}\s\d{1,2}', ' ', text) # Removes ^BEIJING, Jan 26
-    text = re.sub(r'^[a-zA-Z]+\s\(Reuters\)', ' ', text)
 
     text = text.replace('\n', ' ')
     text = re.sub(r'\((.*?)\)',' ', text) # Remove ('text')
