3143e606eb52e40fde19d526b001c65d993690dd
asa10e <31635204+asa10e@users.noreply.github.com>
2018-11-08 12:02:06 +0800

    Handling of very important events such as FOMC

--- SpacyMethod.py
+++ SpacyMethod.py
@@ -16,11 +16,13 @@ logger = logging.getLogger()
 import country_converter as country_converter
 
 def coco(name):
-    '''
+    """
     'U.S.','United States of America' -> 'United States', etc.
-    '''
+    """
     # Remove a leading "the" from names. Note that even for non-countries this is desirable for readability.
     name = re.sub(r'^(the\s)', '', name, flags=re.IGNORECASE)
+    # Remove ending "'s" from names, which is present for some
+    name = re.sub(r'''('s)$''', '', name)
 
     # Custom replacements that country_converter can't seem to handle
     custom_dic = {'US': 'United States','UK':'United Kingdom','EU':'European Union',
@@ -39,13 +41,25 @@ def coco(name):
 stupid_topics = ['New York Times','Fox Business Network','CNN',
                 'North','South','East','West']
 
+# If these terms appear in the text, we will definitely include them as important topics
+very_important_events = ['FOMC','Federal Open Market Committee',
+                         'Jackson Hole',
+                         'Trade War','Trade Tension','Protectionism',
+                         'Brexit',
+                         'Monetary Policy']
+
+# Some standardizations
+replacements = {'U.S. Federal Reserve':'Fed', 'Federal Reserve':'Fed',
+                'Federal Open Market Committee':'FOMC',
+                'Trade Tension':'Trade War','Protectionism':'Trade War'}
+
 def sp_text(text):
-    '''
+    """
     Outputs a dataframe of length <= 10 of tag, type, and score.
     The tag is a specific Person, Location, Event, or Other mentioned in the text.
     The type is literally either Person, Location, Event, or Other.
     The score is its normalized number of appearances (including mentions).
-    '''
+    """
 
     text = quick_clean(text) # Clean the text up
     doc = nlp(text)
@@ -66,18 +80,36 @@ def sp_text(text):
     df['type'] = [readable_types[w.label_] for w in doc.ents if w.label_ in good_labels if not w.text.isspace()]
     df['score'] = [df['tag'].values.tolist().count(e) for e in df['tag'].values.tolist()]
 
-    df = df.drop_duplicates('tag') # Remove duplicates
-    df = df.sort_values(by=['score'], ascending = False) # Sort by score
-
-    df['tag'] = df.tag.apply(coco) # Standardize country names. Non-country names will not be affected
+    # Standardize country names
+    # Non-country names will not be affected other than a little cleaning (e.g. removing '^the')
+    df['tag'] = df.tag.apply(coco)
 
     df = df[~df.tag.isin(stupid_topics)] # Remove stupid topics
+
+    # Push to the top of df very_important_events, if they are in the text
+    top_score = max(df.score)
+    for e in very_important_events:
+        if e.lower() in text.lower():
+            new_row = [e, 'Event', top_score]
+            df.loc[-1] = new_row # Create new row with index -1 (ensures a new index)
+            # Now we shift the index up one, so -1 becomes 0
+            # This is done before removing duplicates so that these very important events are not removed
+            # They are classified as 'Event' and we don't want that to change.
+            df.index = df.index + 1
+            df = df.sort_index() # Push the important event to the top since it is now 0
+
+    # Some custom replacements
+    for r in replacements.keys():
+        df = df.replace(r, replacements[r])
+
+    df = df.drop_duplicates('tag') # Remove duplicates
+    df = df.sort_values(by=['score'], ascending = False) # Sort by score
 
     # If both 'Jamal Khashoggi' and 'Khashoggi' are tags, we only want the longer of the two.
     # Similarly, we care about 'Saudi Arabia–United States relations' more than 'Saudi Arabia'.
     tag_lis = df.tag.values.tolist()
-    tag_string = ' '.join(tag_lis) # Combined string of all tags
-    new_tag_lis = [t for t in tag_lis if tag_string.count(t)>1] # Only the tags that appear more than once
+    tag_string = ' '+' '.join(tag_lis)+' ' # Combined string of all tags
+    new_tag_lis = [t for t in tag_lis if tag_string.count(' '+t+' ')>1] # Only the tags that appear more than once
     df = df[~df.tag.isin(new_tag_lis)] # Remove those shorter tags
 
     df['score'] = df.score.apply(lambda x: round(x/max(df.score), 2)) # Normalize score
