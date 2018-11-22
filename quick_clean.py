527c1a4fa85f3f8b76d7f7ff879682c9cf4531a4
asa10e <31635204+asa10e@users.noreply.github.com>
2018-11-22 12:13:22 +0800

    Increased Bloomberg handling

--- quick_clean.py
+++ quick_clean.py
@@ -4,8 +4,12 @@ def quick_clean(text):
     text = re.sub(r'\s+', ' ', text) # Get rid of extra whitespace and newlines
     text = re.sub(r'^(.*\(Reuters\))','',text) # Just get rid of everything before the (Reuters)
     text = re.sub(r'^(.*\(Bloomberg\))','',text) # Just get rid of everything before the (Bloomberg)
-
     text = text.replace('For best results when printing this announcement, please click on link below:', ' ')
+
+    # These subs get rid of the reporter and editor names at the end of Bloomberg articles
+    text = re.sub(r'(To contact the reporters on this story:.*)','',text, flags=re.DOTALL)
+    text = re.sub(r'(-With assistance from.*)','',text, flags=re.DOTALL)
+    text = re.sub(r'(Read morehere.*)','',text, flags=re.DOTALL)
 
     text = text.replace('\n', ' ')
     text = re.sub(r'\((.*?)\)',' ', text) # Remove ('text')
